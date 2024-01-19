1714446d652baebcc4699df1355eafbfc0427221
Tripti Chhetri <triptichhetri312@gmail.com>
2024-01-19 14:04:15 -0600

    user signup route completed

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const port = 3000;
-
+const url = process.env.MONGODB_URI;
 const usersRoute = require('./routes/users.js');
 
 app.use(express.json());
 app.use('/users', usersRoute);
+
+mongoose.connect(process.env.MONGODB_URI)
+.catch(e => console.log("mongoose error " + e))
 
 app.post('/', (req, res) => {
     res.send('hello');
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const User = require('../models/user.js')
 
-router.post('/register', (req, res) => {
-    const {username, password} = req.body;   
-    res.json({"message": "user created successfully", username});
-    res.send("user created successfully")
+//route to register user
+router.post('/register', async (req, res) => {
+    const {email, password} = req.body;  
+    const userFound = await User.findOne({email});
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    if(userFound){
+        res.status(401).json({message: "User already exist. Please login"})
+    } else {
+        const newUser = new User({
+            firstname,
+            lastname,
+            email,
+            password: hashedPassword,
+          });          
+        await newUser.save();
+        res.status(200).json({"message": "user created successfully", newUser});
+    }  
+})
+
+//login route
+router.post('/login', async (req, res) => {
+    
 })
 
 module.exports = router;
