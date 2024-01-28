23519a57ffad2f088a715fa76d79dcbbcb821c6b
Tripti Chhetri <triptichhetri312@gmail.com>
2024-01-27 23:36:55 -0600

    login route setup

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,19 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('../models/user.js')
+const jwt = require('jsonwebtoken');
+const secretkey = process.env.SECRET_KEY
+
+
+
+//generate token on login
+const generateToken = (email, userId) => {
+    return jwt.sign({email, userId}, secretkey, {expiresIn: '24h'});
+}
 
 //route to register user
 router.post('/register', async (req, res) => {
-    const {email, password} = req.body;  
+    const {firstname, lastname, email, password} = req.body;  
     const userFound = await User.findOne({email});
     const hashedPassword = await bcrypt.hash(password, 10);
 
@@ -25,7 +34,24 @@ router.post('/register', async (req, res) => {
 
 //login route
 router.post('/login', async (req, res) => {
-    
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+
+    if(user){
+        if(await bcrypt.compare(password, user.password)){
+            console.log('got it');
+            const token = generateToken(email, user._id);
+            res.status(200).json({"message": "user is successfully logged in", token, user});
+        } else {
+            console.log("password doesn't match");
+            res.status(401).json({"message": "Username/Password doesn't match"});
+        }       
+    } else {
+        res.status(403).json({"message": "User doesn't exist"});
+    }
 })
+
+
+
 
 module.exports = router;
